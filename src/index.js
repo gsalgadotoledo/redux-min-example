95176ab4d014de60d9876ffa30e8fe661f759b3f
Gustavo Salgado <gsalgadotoledo@gmail.com>
2019-01-26 17:25:37 -0500

    Example

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Aqui se renderiza por primera vez el componente
+const App = (props) => <h1>Hello... {props.count} </h1>
+ReactDOM.render(<App count="0" />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// STATE: Son los datos de la aplicacion
+
+// Aqui se declara como se debe modificar 
+// el STATE cada vez que invocan una accion
+const reducer = (state, action) => {
+  if (action.type === 'SUMAR') {
+    return state + action.count;
+  }
+  return state;
+};
+
+// El store es donde se definen los reducers, solo pasa solo una vez
+const store = createStore(reducer, 0);
+
+// Cada vez que alguien ahga un dispatch se ejecua el suscribe
+store.subscribe(() => {
+  console.warn('New dispatch', store.getState());
+  ReactDOM.render(<App count={store.getState()} />, document.getElementById('root'));
+});
+
+setInterval(() => {
+  store.dispatch({ type: 'SUMAR', count: 1 });
+}, 1000)
